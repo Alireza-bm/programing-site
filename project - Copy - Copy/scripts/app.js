a0d69f6755ecfcca5aed01de2e63bf3df7df45a0
Alireza <134637561+Alireza-bm@users.noreply.github.com>
2023-11-11 14:31:21 +0330

    Add files via upload

---
+++ project - Copy - Copy/scripts/app.js
@@ -0,0 +1,8 @@
+let menu = document.getElementsByClassName("menu")
+let menuBtn = document.getElementsByClassName("icon-tem")
+let menuicon = document.querySelector(".icon-tem  i")
+
+menuBtn.addEventListener("click",function(){
+    console.log("daewfdwaef wea")
+})
+alert("hello")
